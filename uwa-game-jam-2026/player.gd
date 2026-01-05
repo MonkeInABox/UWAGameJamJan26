7a69dd7b57a155f62398c7f24da1adeb7024cfc2
SwordOfTheNebulae <49628441+SwordOfTheNebulae@users.noreply.github.com>
2026-01-05 19:45:25 +0800

    iso tilemap test & time rewind test

---
+++ uwa-game-jam-2026/player.gd
@@ -0,0 +1,60 @@
+extends Node2D
+
+@export var speed := 250.0
+@export var max_time_ms := 30.0 * 1000.0
+@export var reset_time_ms := 1.5 * 1000.0
+# at most every X seconds, save the current position
+# accumulator is reset every time, so it is inaccurate (on purpose kinda)
+@export var position_capture_time := 0.1
+var position_capture_accumulator := 0.0
+
+@onready var timer := Time.get_ticks_msec()
+@onready var reset_pos := self.position
+var old_positions: PackedVector2Array = []
+var state := STATE_NORMAL
+
+@export var timer_label: TimerDisplay
+
+enum {
+	STATE_NORMAL,
+	STATE_RESETTING,
+}
+
+func _ready() -> void:
+	timer_label.max_value = max_time_ms / 1000.0
+
+func _process(delta: float) -> void:
+	var now := Time.get_ticks_msec()
+	match self.state:
+		STATE_NORMAL:
+			var input_dir := Input.get_vector("left", "right", "up", "down")
+			# temporary
+			self.position += input_dir * speed * delta
+			
+			self.position_capture_accumulator += delta
+			if self.position_capture_accumulator > self.position_capture_time:
+				self.position_capture_accumulator = 0
+				self.old_positions.append(self.position)
+			
+			if self.timer + max_time_ms <= now:
+				self.timer = now
+				self.state = STATE_RESETTING
+				timer_label.value = 0.0
+			else:
+				timer_label.value = (max_time_ms - (now - timer)) / 1000.0
+		STATE_RESETTING:
+			var reset_amount := (now - timer) / reset_time_ms
+			if reset_amount >= 1.0:
+				self.timer = now
+				self.state = STATE_NORMAL
+				timer_label.value = max_time_ms / 1000.0
+				self.position = reset_pos
+				self.old_positions.clear()
+			else:
+				var index_f := (1.0 - reset_amount) * self.old_positions.size()
+				var index := int(index_f)
+				var lerp_amount = index_f - index
+					
+				self.position = self.old_positions[0] if index == 0 else self.old_positions[index-1].lerp(self.old_positions[index], lerp_amount)
+				timer_label.value = reset_amount * max_time_ms / 1000.0
+	timer_label.queue_redraw()
---
+++ uwa-game-jam-2026/timer_display.gd
@@ -0,0 +1,35 @@
+class_name TimerDisplay extends Node2D
+
+
+var value := 30.0
+var max_value: float = 30: 
+	set(value):
+		max_value = value
+		colour_threshold_1 = max_value / 2.0
+		colour_threshold_2 = max_value / 4.0
+	get():
+		return max_value
+@export var width := 128.0
+
+var colour_threshold_1 := max_value / 2.0
+var colour_threshold_2 := max_value / 4.0
+
+@export var font: Font
+@export var colour1: Color
+@export var colour2: Color
+@export var colour3: Color
+
+const font_size := 16
+const padding_top := 4.0
+const padding_middle := 4.0
+const bar_height := 4.0
+
+func _draw() -> void:
+	var viewport := get_viewport_rect()
+	var center := viewport.size.x / 2.0
+	self.draw_string(font, Vector2(center - width/2.0, font_size + padding_top), "%.2fs" % self.value, HORIZONTAL_ALIGNMENT_CENTER, width, font_size)
+	var colour := (colour1 if value > colour_threshold_1 else 
+			colour1.lerp(colour2, 1 - (value - colour_threshold_2) / (colour_threshold_1 - colour_threshold_2))
+			if value > colour_threshold_2 else	colour2.lerp(colour3, 1 - value / colour_threshold_2)
+		)
+	self.draw_rect(Rect2(center - width / 2.0, font_size + padding_top + padding_middle, value * width / max_value, bar_height), colour)
