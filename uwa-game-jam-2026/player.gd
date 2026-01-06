8eb2e82b08704f7abfe182f06b2a975dc9f83bbe
SwordOfTheNebulae <49628441+SwordOfTheNebulae@users.noreply.github.com>
2026-01-06 12:38:46 +0800

    changed player movement
     - added momentum
     - made the player slide along walls.
      - i was going to use the `move_and_slide` method, but that causes the player to slide too fast along the walls, since its really meant for side scrolling games, so i copied the c++ code for it into gdscript and modified it
     - also changed the collision shapes for the player and walls
      - collision moved to the floor area only, which looks better with the fake isometric perspective

--- uwa-game-jam-2026/player.gd
+++ uwa-game-jam-2026/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 @export var speed := 250.0
+@export var accel := 2500.0
 
 @onready var time_manager: TimeManager = %"time manager"
 
@@ -10,5 +11,33 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	if time_manager.state == TimeManager.STATE_NORMAL:
 		var input_dir := Input.get_vector("left", "right", "up", "down")
-		self.velocity = input_dir * speed * delta
-		move_and_collide(self.velocity)
+		self.velocity += input_dir * accel * delta * 0.5
+		self.velocity *= 0.85 if input_dir.is_zero_approx() else 0.95
+		#if self.velocity.length_squared() >= speed * speed: self.velocity = input_dir * speed
+		var current_speed_squared = self.velocity.length_squared()
+		if current_speed_squared >= speed * speed: self.velocity *= speed / sqrt(current_speed_squared)
+		
+		# code taken and modified from the godot c++ source for CharacterBody2D::move_and_slide()
+		var motion := self.velocity * delta
+		for _iter in 4:
+			var collision := move_and_collide(motion)
+			if collision == null:
+				break
+			if collision.get_remainder().is_zero_approx():
+				motion = Vector2()
+				break
+			if collision.get_angle(-self.velocity.normalized()) < deg_to_rad(15.0):
+				motion = Vector2()
+			else:
+				motion = collision.get_remainder().slide(collision.get_normal())
+				
+			if motion.dot(self.velocity) <= 0.0:
+				motion = Vector2()
+				
+			if motion.is_zero_approx():
+				break
+
+		self.velocity += input_dir * accel * delta * 0.5
+	else:
+		self.velocity = Vector2()
+		
